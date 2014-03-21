41a74fac137a63982e492e095229c8cb8bebc5e9
Abdul Chaudhry <abdollar@yahoo.com>
2014-03-21 14:54:47 -0700

    added sts (security token service) and GetFederationToken

---
+++ sts/federation_token.go
@@ -0,0 +1,141 @@
+package sts
+
+import (
+	"bytes"
+	"encoding/xml"
+	"fmt"
+	"github.com/abdollar/goamz/aws"
+	"io"
+	"net/http"
+)
+
+const debug = true
+
+type Sts struct {
+	aws.Auth
+	aws.Region
+}
+
+type xmlErrors struct {
+	RequestId string  `xml:"RequestID"`
+	Errors    []Error `xml:"Errors>Error"`
+}
+
+// Error contains pertinent information from the failed operation.
+type Error struct {
+	// HTTP status code (200, 403, ...)
+	StatusCode int
+	// AutoScaling error code ("UnsupportedOperation", ...)
+	Code string
+	// The human-oriented error message
+	Message   string
+	RequestId string `xml:"RequestID"`
+}
+
+func (err *Error) Error() string {
+	if err.Code == "" {
+		return err.Message
+	}
+
+	return fmt.Sprintf("%s (%s)", err.Message, err.Code)
+}
+
+// Factory for the Sts type
+func NewSts(auth aws.Auth, region aws.Region) (*Sts, error) {
+	return &Sts{
+		Auth:   auth,
+		Region: region,
+	}, nil
+}
+
+func (sts *Sts) query(params map[string]string, resp interface{}) error {
+	params["Version"] = "2011-06-15"
+	params["Timestamp"] = timeNow().In(time.UTC).Format(time.RFC3339)
+	endpoint, err := url.Parse(sts.Region.StsEndpoint)
+	if err != nil {
+		return err
+	}
+	sign(sts.Auth, "GET", endpoint.Path, params, endpoint.Host)
+	endpoint.RawQuery = multimap(params).Encode()
+	if debug {
+		log.Printf("get { %v } -> {\n", endpoint.String())
+	}
+	r, err := http.Get(endpoint.String())
+	if err != nil {
+		return err
+	}
+	defer r.Body.Close()
+
+	if debug {
+		dump, _ := httputil.DumpResponse(r, true)
+		log.Printf("response:\n")
+		log.Printf("%v\n}\n", string(dump))
+	}
+	if r.StatusCode != 200 {
+		return buildError(r)
+	}
+	err = xml.NewDecoder(r.Body).Decode(resp)
+	return err
+}
+
+func multimap(p map[string]string) url.Values {
+	q := make(url.Values, len(p))
+	for k, v := range p {
+		q[k] = []string{v}
+	}
+	return q
+}
+
+func addParamsList(params map[string]string, label string, ids []string) {
+	for i, id := range ids {
+		params[label+"."+strconv.Itoa(i+1)] = id
+	}
+}
+
+func buildError(r *http.Response) error {
+	errors := xmlErrors{}
+	xml.NewDecoder(r.Body).Decode(&errors)
+	var err Error
+	if len(errors.Errors) > 0 {
+		err = errors.Errors[0]
+	}
+	err.RequestId = errors.RequestId
+	err.StatusCode = r.StatusCode
+	if err.Message == "" {
+		err.Message = r.Status
+	}
+	return &err
+}
+
+// ----------------------------------------------------------------------------
+// STS base types and related functions.
+
+type Credentials struct {
+	SessionToken    string `xml:"SessionToken"`
+	SecretAccessKey string `xml:"SecretAccessKey"`
+	Expiration      string `xml:"Expiration"`
+	AccessKeyId     string `xml:"AccessKeyId"`
+}
+
+type FederatedUser struct {
+	Arn             string `xml:"Arn"`
+	FederatedUserId string `xml:"FederatedUserId"`
+}
+
+// GetFederationTokenResp defines the basic response structure.
+type GetFederationTokenResp struct {
+	RequestId     string        `xml:"ResponseMetadata>RequestId"`
+	Credentials   Credentials   `xml:"GetFederationTokenResult>Credentials"`
+	FederatedUser FederatedUser `xml:"GetFederationTokenResult>FederatedUser"`
+}
+
+func (sts *Sts) GetFederationToken(durationSeconds int) (resp *GetFederationTokenResponse, err error) {
+	resp = &GetFederationTokenResponse{}
+	params["Action"] = "GetFederationToken"
+	params["DurationSeconds"] = strconv.Itoa(durationSeconds)
+	err = as.query(params, resp)
+	if err != nil {
+		return nil, err
+	}
+	return resp, nil
+}
